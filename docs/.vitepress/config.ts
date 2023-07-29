53f848894a5d1aa2e20bf3b99f04cbbe35820dc9
shuibuzhuo <2301140231@qq.com>
2023-07-29 09:42:18 +0800

    feat: 增加分类

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -8,18 +8,33 @@ export default defineConfig({
     // https://vitepress.dev/reference/default-theme-config
     nav: [
       { text: 'Home', link: '/' },
-      { text: 'Examples', link: '/markdown-examples' }
+      { text: '分类', items: [
+          { text: 'Vue', link: '/vue/markdown-examples'},
+          { text: 'React', link: '/react/markdown-examples3'},
+        ] 
+    }
     ],
 
-    sidebar: [
-      {
-        text: 'Examples',
-        items: [
-          { text: 'Markdown Examples', link: '/markdown-examples' },
-          { text: 'Runtime API Examples', link: '/api-examples' }
-        ]
-      }
-    ],
+    sidebar: {
+      '/vue/': [
+        {
+          text: '文章',
+          items: [
+            { text: '文章1', link: '/vue/markdown-examples' },
+            { text: '文章2', link: '/vue/markdown-examples2' },
+          ]
+        }
+      ],
+      '/react/': [
+        {
+          text: '文章',
+          items: [
+            { text: '文章3', link: '/react/markdown-examples3' },
+            { text: '文章4', link: '/react/markdown-examples4' },
+          ]
+        }
+      ],
+    },
 
     socialLinks: [
       { icon: 'github', link: 'https://github.com/vuejs/vitepress' }
--- docs/vue/markdown-examples.md
+++ docs/vue/markdown-examples.md
@@ -1,4 +1,4 @@
-# Markdown Extension Examples 200
+# Markdown Extension Examples - Vue
 
 This page demonstrates some of the built-in markdown extensions provided by VitePress.
 

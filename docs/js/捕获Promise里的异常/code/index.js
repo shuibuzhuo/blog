7e1495013d1e20cc919296b02fda79082ac5c0f3
shuibuzhuo <2301140231@qq.com>
2024-01-30 17:02:24 +0800

    捕获错误增加 setTimeout case

--- docs/js/捕获Promise里的异常/code/index.js
+++ docs/js/捕获Promise里的异常/code/index.js
@@ -2,25 +2,33 @@
   try {
     console.log(100);
 
-    throw new Error("try error");
+    // throw new Error("try error");
 
-    const p1 = Promise.resolve();
+    setTimeout(() => {
+      try {
+        throw new Error("setTimeout error");
+      } catch (error) {
+        console.log("setTimeout的错误捕获到了", error);
+      }
+    }, 1000);
 
-    p1.then(() => {
-      console.log("p1 then");
-      throw new Error("p1 then error");
-    }).catch((e) => {
-      console.log("p1 e", e.message);
-    });
+    // const p1 = Promise.resolve();
 
-    const p2 = Promise.resolve();
+    // p1.then(() => {
+    //   console.log("p1 then");
+    //   throw new Error("p1 then error");
+    // }).catch((e) => {
+    //   console.log("p1 e", e.message);
+    // });
 
-    try {
-      await p2;
-      throw new Error("p2 error");
-    } catch (e) {
-      console.log("p2 e", e.message);
-    }
+    // const p2 = Promise.resolve();
+
+    // try {
+    //   await p2;
+    //   throw new Error("p2 error");
+    // } catch (e) {
+    //   console.log("p2 e", e.message);
+    // }
   } catch (e) {
     console.log("e", e.message);
   }

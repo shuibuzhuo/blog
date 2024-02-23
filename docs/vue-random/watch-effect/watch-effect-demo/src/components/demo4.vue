2cc35e962b76c66954c51aec72882a4c2934b291
shuibuzhuo <2301140231@qq.com>
2024-02-24 00:45:02 +0800

    副作用函数执行顺序

--- docs/vue-random/watch-effect/watch-effect-demo/src/components/demo4.vue
+++ docs/vue-random/watch-effect/watch-effect-demo/src/components/demo4.vue
@@ -1,13 +1,31 @@
 <template>
   <div>
     <h1>副作用执行顺序</h1>
+    <div ref="node">{{ greeting }}</div>
+    <button @click="append">增加</button>
   </div>
 </template>
 
 <script setup lang="ts">
-import { watchEffect } from "vue";
+import { ref, watchEffect } from "vue";
 
-watchEffect(() => {
-  console.log("effect run");
-});
+const greeting = ref("hi");
+const count = ref(0);
+function append() {
+  greeting.value += "hi";
+  count.value++;
+}
+const node = ref<HTMLElement | null>(null);
+watchEffect(
+  () => {
+    console.log("effect run");
+    console.log("count", count.value);
+
+    const currentText = node.value ? node.value.innerText : "";
+    console.log("currentText", currentText);
+  },
+  {
+    flush: "post",
+  }
+);
 </script>
